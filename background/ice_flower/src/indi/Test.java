37603033a3f73c914e4802dd8e53829d980f3792
string <493615514@qq.com>
2018-03-26 23:12:21 +0800

    推荐算法
    协同

---
+++ background/ice_flower/src/indi/Test.java
@@ -0,0 +1,27 @@
+package indi.ice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test {
+	public static void main(String args[]){  
+		/*
+		user and item matrix,竖着user，0 means the user hadn't buy the item,
+		
+		ColFlt aptest=new ColFlt(new int[][] {
+				   new int[]{1,0,0,0,5},
+				   new int[]{1,2,0,4,0},
+				   new int[]{0,0,0,1,2},
+				   new int[]{3,5,0,1,5},
+				   new int[]{3,0,1,0,0} 
+		}); 
+		//输入用户序号，即可能推荐的item，返回单个最优，未测试效率，后续优化
+		int item=aptest.getItems(1);
+		System.out.println(item);
+		
+		Apriori ap=new Apriori();
+		ap.ma(2);
+		*/
+	
+	}  
+}
