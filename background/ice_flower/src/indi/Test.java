407d1323f02d0ecdd065171471dc620d2c34ef69
nobody <493615514@qq.com>
2018-03-28 22:48:50 +0800

    important less

--- background/ice_flower/src/indi/Test.java
+++ background/ice_flower/src/indi/Test.java
@@ -1,12 +1,11 @@
-package indi.ice;
+package indi;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
 	public static void main(String args[]){  
-		/*
-		user and item matrix,竖着user，0 means the user hadn't buy the item,
+		/*The matrix of user and item, user field 0 means the user hadn't buy the item*/
 		
 		ColFlt aptest=new ColFlt(new int[][] {
 				   new int[]{1,0,0,0,5},
@@ -15,13 +14,9 @@ public class Test {
 				   new int[]{3,5,0,1,5},
 				   new int[]{3,0,1,0,0} 
 		}); 
-		//输入用户序号，即可能推荐的item，返回单个最优，未测试效率，后续优化
+	
 		int item=aptest.getItems(1);
 		System.out.println(item);
-		
-		Apriori ap=new Apriori();
-		ap.ma(2);
-		*/
-	
+			
 	}  
 }
