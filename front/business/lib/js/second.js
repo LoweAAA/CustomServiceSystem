4a5470f755b3108563dacca84f4189220f83513e
Shawn <418954816@qq.com>
2018-04-02 00:01:38 +0800

    添加了各页面的选择框以及首页展示内容初步样式

--- front/business/lib/js/second.js
+++ front/business/lib/js/second.js
@@ -39,19 +39,17 @@ $(function(){
                     type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
                 }
             },
-            legend: {
-                data:['总访问量','A机型','B机型','M手机','K手机','iPad','Watch','搜索引擎','其他']
-            },
+           
             grid: {
-                left: '3%',
-                right: '4%',
+                left: '10%',
+                right: '10%',
                 bottom: '3%',
                 containLabel: true
             },
             xAxis : [
                 {
                     type : 'category',
-                    data : ['周一','周二','周三','周四','周五','周六','周日']
+                    data : ['总访问量','A机型','B机型','M手机','K手机','iPad','其他']
                 }
             ],
             yAxis : [
@@ -59,71 +57,14 @@ $(function(){
                     type : 'value'
                 }
             ],
-            series : [
+            series : 
                 {
                     name:'总访问量',
                     type:'bar',
                     data:[320, 332, 301, 334, 390, 330, 320]
-                },
-                {
-                    name:'A机型',
-                    type:'bar',
-                    stack: '广告',
-                    data:[120, 132, 101, 134, 90, 230, 210]
-                },
-                {
-                    name:'B机型',
-                    type:'bar',
-                    stack: '广告',
-                    data:[220, 182, 191, 234, 290, 330, 310]
-                },
-                {
-                    name:'M手机',
-                    type:'bar',
-                    stack: '广告',
-                    data:[150, 232, 201, 154, 190, 330, 410]
-                },
-                {
-                    name:'K手机',
-                    type:'bar',
-                    data:[862, 1018, 964, 1026, 1679, 1600, 1570],
-                    markLine : {
-                        lineStyle: {
-                            normal: {
-                                type: 'dashed'
-                            }
-                        },
-                        data : [
-                            [{type : 'min'}, {type : 'max'}]
-                        ]
-                    }
-                },
-                {
-                    name:'iPad',
-                    type:'bar',
-                    barWidth : 5,
-                    stack: '搜索引擎',
-                    data:[620, 732, 701, 734, 1090, 1130, 1120]
-                },
-                {
-                    name:'Watch',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[120, 132, 101, 134, 290, 230, 220]
-                },
-                {
-                    name:'搜索引擎',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[60, 72, 71, 74, 190, 130, 110]
-                },
-                {
-                    name:'其他',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[62, 82, 91, 84, 109, 110, 120]
                 }
-            ]
+               
+        
         };
         if (option && typeof option === "object") {
             myChart.setOption(option, true);
