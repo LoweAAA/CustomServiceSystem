6843f6aef96a3d306bd41790c7266048738d5d58
woopeer <woop.moom@gmail.com>
2018-05-06 12:19:37 +0800

    first commit

---
+++ ret/ret.java
@@ -0,0 +1,9 @@
+import org.python.util.PythonInterpreter;
+public class ret {
+	public static void main(String[] args) {
+		PythonInterpreter ip = new PythonInterpreter();
+		
+		
+		System.out.println("hello, world!");
+	}
+}
