ba213ea2637f551ee65f10aab67f22868cd52b3a
woopeer <woop.moom@gmail.com>
2018-05-14 19:48:00 +0800

    DB NEEDED

--- ret/ret.java
+++
@@ -1,9 +0,0 @@
-import org.python.util.PythonInterpreter;
-public class ret {
-	public static void main(String[] args) {
-		PythonInterpreter ip = new PythonInterpreter();
-		
-		
-		System.out.println("hello, world!");
-	}
-}
