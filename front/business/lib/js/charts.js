736b848eca49c5ba7525afdf628ac5004b69f39b
Shawn <418954816@qq.com>
2018-08-03 07:30:35 +0800

    整合

--- front/business/lib/js/charts.js
+++ front/business/lib/js/charts.js
@@ -1,5 +1,5 @@
 $(function(){
-//产品问题  
+//产品问题	
 var option1 = {
     title: {
         text:'产品问题',
@@ -39,8 +39,8 @@ var option1 = {
     ]
 };
 
-//关键词
-  var option2 = {
+	//关键词
+	var option2 = {
     title: {
     text: '关键词统计',
     top:'10%',
@@ -86,8 +86,9 @@ series : {
     }
 }
 };
-//客服评价
-  
+
+	//客服评价
+	
 option3 = {
     title: {
         text: '客服评价统计',
@@ -101,7 +102,7 @@ option3 = {
     yAxis:{
         name:'评分',
         scale:true,
-      min:function(value) {return (value.min/20).toFixed(0)*10;}
+	    min:function(value) {return (value.min/20).toFixed(0)*10;}
     },
     dataZoom: {   
         type: 'slider', 
@@ -137,43 +138,6 @@ option3 = {
         barMinHeight:50
     }
 };
-var myChart1 = echarts.init(document.getElementById('production'));
-var myChart2 = echarts.init(document.getElementById("keywords"));
-var myChart3 = echarts.init(document.getElementById('Evaluation'));
-var count1=0;  
-var count2=0;  
-myChart1.setOption(option1);
-myChart2.setOption(option2);
-myChart3.setOption(option3);
-$(document).ready(function(){
-  $.post("118.25.100.232/get.php",{type:1},function(data,status){
-      datas=[['production', 'last  month', 'two month ', 'three month ']].concat(JSON.parse(data).map(function(e){
-        return [e.pro_id,e.one_ago,e.two_ago,e.three_ago];}));   
-      myChart1.setOption({
-          dataset: {
-              source: datas
-      }});
-  });
-    $.post("118.25.100.232/get.php",{type:2},function(data,status){
-        datas=JSON.parse(data).map(function(e){ return {name:e.word_class,value:e.one_ago};});  
-        myChart2.setOption({
-            series : {
-                name:'关键词',
-                data:datas, 
-            }}); });  
-    $.post("118.25.100.232/get.php",{type:3},function(data,status){
-      datas=[['servicer','socre']].concat(JSON.parse(data).map(function(e){
-             return [e.service_id,e.one_ago];}));   
-        myChart3.setOption({
-          dataset: {
-                source: datas
-              }}); });
-    $.post("118.25.100.232/get.php",{type:4},function(data,status){
-        count1=JSON.parse(data)[0].online_count; //在线客服数
-        count2=JSON.parse(data)[0].view_count;   //访问量
-        //document.get……
-    });
 
 
-  }); 
 });
